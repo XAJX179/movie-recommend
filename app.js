e013fe78d1fe752de2407bdc4e9216fa9a886c8f
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-10-25 15:00:57 +0530

    Feat : Clear list & Loading text

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const form = document.getElementById('movie-form');
+const genre = document.getElementById('genre')
+const rating = document.getElementById('rating')
+const year = document.getElementById('year')
+const apikey = 'ad7ef549b9a6fdbb99f3581ba04b9bee'
+const moviesList = document.getElementById('movies-list');
+
+const loadingText = document.createElement('li');
 
 form.addEventListener('submit', (e) => handle_submit(e))
 
 function handle_submit(e) {
+  e.preventDefault();
+  clear_list();
+  loading_text();
+}
 
+function clear_list() {
+  moviesList.innerText = '';
+}
+
+function loading_text() {
+  loadingText.textContent = 'Loading recommendations...';
+  moviesList.appendChild(loadingText);
 }

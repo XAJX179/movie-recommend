8eaef769066b93ccfc32f79cf42971f0e1a063f4
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-10-25 16:56:10 +0530

    Fix : ReferenceError and update url values

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const form = document.getElementById('movie-form');
-const genre = document.getElementById('genre')
-const rating = document.getElementById('rating')
-const year = document.getElementById('year')
-const apiKey = 'ad7ef549b9a6fdbb99f3581ba04b9bee'
+const apiKey = 'ad7ef549b9a6fdbb99f3581ba04b9bee';
 const moviesList = document.getElementById('movies-list');
-
-const loadingText = document.createElement('li');
-let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}&vote_average.gte=${rating}`;
-if (year) {
-  url += `&primary_release_year=${year}`;
-}
 
 form.addEventListener('submit', (e) => handleSubmit(e))
 
 function handleSubmit(e) {
   e.preventDefault();
+  const genre = document.getElementById('genre').value;
+  const rating = document.getElementById('rating').value;
+  const year = document.getElementById('year').value;
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}&vote_average.gte=${rating}`;
+  if (year) {
+    url += `&primary_release_year=${year}`;
+  }
   clearList();
   loadingMessage();
-  getRecommendations();
+  getRecommendations(url, moviesList);
 }
 
 function clearList() {
@@ -25,11 +23,12 @@ function clearList() {
 }
 
 function loadingMessage() {
+  const loadingText = document.createElement('li');
   loadingText.textContent = 'Loading recommendations...';
   moviesList.appendChild(loadingText);
 }
 
-function getRecommendations() {
+function getRecommendations(url, moviesList) {
   fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -43,15 +42,15 @@ function getRecommendations() {
 
       }
       else {
-
+        console.log('nope')
       }
     })
     .catch(error => {
       console.error('Error fetching recommendations...', error);
-      errorMessage();
+      errorMessage(moviesList);
     })
 }
 
-function errorMessage() {
+function errorMessage(moviesList) {
   moviesList.innerHTML = '<li>Error fetching recommendations. Please try again later.</li>';
 }

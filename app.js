198f4104311e383022ece0c399fe5dd6c99bf1b1
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-10-25 18:20:10 +0530

    Feat : Convert fetched data to html using DOM

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function handleSubmit(e) {
 }
 
 function clearList() {
-  moviesList.innerText = '';
+  moviesList.innerHTML = '';
 }
 
 function loadingMessage() {
@@ -39,18 +39,45 @@ function getRecommendations(url, moviesList) {
     .then(data => {
       clearList();
       if (data.results.length > 0) {
+        data.results.forEach(movie => {
+          const li = document.createElement('li');
+          const movieImg = document.createElement('img');
+          movieImg.src = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'https://via.placeholder.com/100x150?text=No+Image';
+          movieImg.alt = `${movie.title} Poster`
 
+          const movieInfo = document.createElement('div');
+          const movieTitle = document.createElement('p');
+          movieTitle.textContent = `${movie.title}`
+          const movieReleaseDate = document.createElement('p');
+          movieReleaseDate.textContent = `${movie.release_date.split('-')[0]}`
+          const movieRating = document.createElement('p');
+          movieRating.textContent = `⭐ ${movie.vote_average}`
+
+          movieInfo.appendChild(movieTitle);
+          movieInfo.appendChild(movieReleaseDate);
+          movieInfo.appendChild(movieRating);
+
+          li.appendChild(movieImg);
+          li.appendChild(movieInfo);
+
+          moviesList.appendChild(li);
+        })
       }
       else {
-        console.log('nope')
+        const li = document.createElement('li');
+        li.textContent = 'No recommendations found for the selected criteria.';
+        moviesList.appendChild(li);
       }
     })
     .catch(error => {
+      clearList();
       console.error('Error fetching recommendations...', error);
       errorMessage(moviesList);
     })
 }
 
 function errorMessage(moviesList) {
-  moviesList.innerHTML = '<li>Error fetching recommendations. Please try again later.</li>';
+  const li = document.createElement('li');
+  li.textContent = 'Error fetching recommendations. Please try again later.';
+  moviesList.appendChild(li);
 }
